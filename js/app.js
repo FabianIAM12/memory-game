b63660e2de6800a045e2a8527a4c7711072dff74
FabianIAM12 <fabian.b89@gmail.com>
2018-07-12 20:02:19 +0200

    added initial basic functionality e.g. show cards and much more

--- js/app.js
+++ js/app.js
@@ -10,9 +10,43 @@
  *   - add each card's HTML to the page
  */
 
+let open_cards = [];
+
+function set_action_counter_up() {
+    let value = parseInt(document.getElementsByClassName("moves")[0].textContent);
+    document.getElementsByClassName("moves")[0].innerHTML = value + 1;
+}
+
+function no_match() {
+    let first_cart = document.getElementsByClassName(open_cards[open_cards.length - 1]);
+    let second_cart = document.getElementsByClassName(open_cards[open_cards.length - 2]);
+
+    for (let element of first_cart) {
+        element.parentElement.classList.remove('open', 'show', 'match');
+    }
+    for (let element of second_cart) {
+        element.parentElement.classList.remove('open', 'show', 'match');
+    }
+    open_cards.pop();
+    open_cards.pop();
+}
+
+function do_match() {
+    let already_opened_cards = document.getElementsByClassName(entry);
+    for (let selected_entries of already_opened_cards) {
+        selected_entries.parentElement.classList.add("match");
+    }
+}
+
+function show_up(card_element) {
+    card_element.classList.add('open', 'show');
+    let clicked_class = card_element.childNodes[1].getAttribute("class").split(' ')[1];
+    return clicked_class;
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,18 +59,39 @@ function shuffle(array) {
     return array;
 }
 
-var classname = document.getElementsByClassName("card");
+/* reset all cards */
+let reset_button = document.getElementsByClassName('restart');
+reset_button[0].addEventListener('click', function (){
+    let elements = document.getElementsByClassName("card");
+    for (let element of elements){
+        element.classList.remove('open', 'show', 'match');
+    }
+    open_cards = [];
+}, false);
 
-for (var i = 0; i < classname.length; i++) {
+/* open card deck on click */
+let classname = document.getElementsByClassName("card");
+for (let i = 0; i < classname.length; i++) {
     classname[i].addEventListener('click', function (){
-        this.classList.add("match");
+        set_action_counter_up();
+        let clicked_class = show_up(this);
+        let hit = false;
 
-        console.log(this.childNode[0]);
-        //setTimeout(function(){ alert("Hello"); }, 2000);
-
+        // Iterate cards and check if match
+        for (let entry of open_cards){
+            if (clicked_class === entry) {
+                do_match();
+                hit = true;
+            }
+        }
+        open_cards.push(clicked_class);
+        
+        // Cards do not match
+        if (open_cards.length % 2 === 0 && hit === false){
+            no_match();
+        }
     }, false);
 }
-
 
 /*
 const mainHeading = document.body.querySelectorAll('.card');
@@ -45,8 +100,6 @@ mainHeading.addEventListener('click', function () {
     console.log('The heading was clicked!');
 });
 */
-
-
 
 
 /*
